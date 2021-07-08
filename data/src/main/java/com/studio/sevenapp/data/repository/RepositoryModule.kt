cc3b50ce750b7c6947e1a1fd5a678efca2d07366
isaque.coelho <isaque.coelho@dextra-sw.com>
2021-07-08 01:34:41 -0300

    refactor: change injection from @Providers to @Bind structure to Character Repository;

--- data/src/main/java/com/studio/sevenapp/data/repository/RepositoryModule.kt
+++ data/src/main/java/com/studio/sevenapp/data/repository/RepositoryModule.kt
@@ -1,63 +1,17 @@
 package com.studio.sevenapp.data.repository
 
-import com.google.gson.JsonObject
-import com.studio.sevenapp.data.mapper.CharacterInDetailMapper
-import com.studio.sevenapp.data.mapper.CharacterMapper
-import com.studio.sevenapp.data.service.CharacterService
-import com.studio.sevenapp.domain.character.BodyQueryEnum
 import com.studio.sevenapp.domain.character.CharacterRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.MediaType
-import okhttp3.MediaType.Companion.toMediaType
-import okhttp3.RequestBody
-import okhttp3.RequestBody.Companion.toRequestBody
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
+abstract class RepositoryModule {
 
-    @Singleton
-    @Provides
-    fun provideJsonObject(): JsonObject {
-        return JsonObject()
-    }
-
-    @Singleton
-    @Provides
-    fun provideRequestType(): MediaType {
-        val bodyType = BodyQueryEnum.BODY_TYPE.value
-        return bodyType.toMediaType()
-    }
-
-    @Singleton
-    @Provides
-    fun provideRequestBody(
-        jsonObject: JsonObject,
-        mediaType: MediaType
-    ): RequestBody {
-        return jsonObject.toString()
-            .toRequestBody(mediaType)
-    }
-
-    @Singleton
-    @Provides
-    fun provideCharacterRepository(
-        characterService: CharacterService,
-        jsonObject: JsonObject,
-        mediaType: MediaType,
-        characterMapper: CharacterMapper,
-        characterInDetailMapper: CharacterInDetailMapper
-    ): CharacterRepository {
-        return CharacterRepositoryImpl(
-            characterService = characterService,
-            jsonObject = jsonObject,
-            mediaType = mediaType,
-            characterMapper = characterMapper,
-            characterInDetailMapper = characterInDetailMapper
-        )
-    }
+    @Binds
+    abstract fun bindCharacterRepository(
+        characterRepositoryImpl: CharacterRepositoryImpl
+    ): CharacterRepository
 }
